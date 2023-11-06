e78cd678a80e060540b18ddaff66e33d2ddc5599
motoko-ayanami <122310140+motoko-ayanami@users.noreply.github.com>
2023-11-06 14:20:54 -0500

    Update fb_marketplace.js
    Changed the entire behavior of the script. Added a check that visits links to see if they are actually new.

--- fb_marketplace.js
+++ fb_marketplace.js
@@ -1,28 +1,116 @@
 const puppeteer = require('puppeteer');
 const fs = require('fs');
 const axios = require('axios');
+const { DateTime } = require('luxon');
 
-const USERNAME = '';
-const PASSWORD = '';
-const DISCORD_WEBHOOK_URL = 'https://discord.com/api/webhooks/';
+const DISCORD_WEBHOOK_URL = 'ENTER_DISCORD_WEBHOOK_HERE';
 
 const AD_LINKS = [
-    'https://www.facebook.com/marketplace/category/propertyrentals?minPrice=1800&maxPrice=2500&minBedrooms=2&sortBy=creation_time_descend&exact=false&latitude=45.5415&longitude=-73.6185&radius=1',
-    'https://www.facebook.com/marketplace/category/propertyrentals?minPrice=1800&maxPrice=2500&minBedrooms=2&sortBy=creation_time_descend&exact=false&latitude=45.5174&longitude=-73.5696&radius=1'
+'ENTER_LINKS_HERE'
 ];
+
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+
+async function pauseIfMidnightTo7am() {
+    const nowInEasternTime = DateTime.now().setZone("America/Montreal");
+    const currentHour = nowInEasternTime.hour;
+
+    if (currentHour < 7) {
+        console.log("Pausing script execution because it's between midnight and 7 AM Eastern Time.");
+        const msTill7 = nowInEasternTime.startOf('day').plus({ hours: 7 }).diff(nowInEasternTime, 'milliseconds').milliseconds;
+        await sleep(msTill7);
+    }
+}
+
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+Date.prototype.dst = function() {
+    const jan = new Date(this.getFullYear(), 0, 1).getTimezoneOffset();
+    const jul = new Date(this.getFullYear(), 6, 1).getTimezoneOffset();
+    return Math.min(jan, jul) === this.getTimezoneOffset();
+};
+
+async function declineCookies(page) {
+    try {
+        await page.waitForSelector('[data-testid="cookie-policy-manage-dialog-accept-button"]', { timeout: 5000 });
+        await page.click('[data-testid="cookie-policy-manage-dialog-accept-button"]');
+        console.log('Declined optional cookies.');
+        await page.waitForTimeout(2000); 
+    } catch (error) {
+        console.log('Cookie popup not found or another error occurred while trying to decline cookies.', error);
+    }
+}
 
 async function loginToFacebook(page) {
     await page.goto('https://www.facebook.com/login');
-    await page.type('#email', USERNAME);
-    await page.type('#pass', PASSWORD);
+    await declineCookies(page);
+    await page.type('#email', 'ENTER_EMAIL_HERE');
+    await page.type('#pass', 'ENTER_PASSWORD_HERE');
     await page.click('[name="login"]');
     await page.waitForTimeout(10000);
     await page.waitForSelector('body');
 }
 
+async function checkForBlock(page) {
+    try {
+        const blockedMessage = await page.$eval('body', (bodyEl) => {
+            const text = bodyEl.innerText || bodyEl.textContent;
+            return text.includes("You’re Temporarily Blocked") ? true : false;
+        });
+
+        if (blockedMessage) {
+            console.log("Blocked by Facebook. Sending alert to Discord.");
+            await page.waitForTimeout(3600000);
+            return true;
+        }
+        return false;
+    } catch (error) {
+        console.log("Error checking for block:", error);
+        return false;
+    }
+}
+
+async function sendToDiscord(adLinks) {
+    try {
+        for (const link of adLinks) {
+            await axios.post(DISCORD_WEBHOOK_URL, {
+                content: link
+            });
+            console.log(`Sent new ad ${link} to Discord`);
+            await sleep(1000);
+        }
+    } catch (error) {
+        console.error('Error sending message to Discord', error);
+    }
+}
+
+async function isNewAdvertisement(page, adLink) {
+    await page.goto(adLink);
+    await page.waitForTimeout(5000);
+
+    return await page.evaluate(() => {
+        const bodyText = document.body.innerText || document.body.textContent;
+        const sentences = bodyText.split('.').map(sentence => sentence.trim());
+        for (const sentence of sentences) {
+            if (sentence.includes("listed") && sentence.includes("minutes")) {
+                return true;
+            }
+        }
+        return false;
+    });
+}
+
 async function fetchAdsFromLink(page, adLink) {
     await page.goto(adLink);
     await page.waitForTimeout(10000);
+
+    const isBlocked = await checkForBlock(page);
+    if (isBlocked) return;
 
     const ads = await page.evaluate(() => {
         const listingNodes = document.querySelectorAll('a[tabindex="0"]');
@@ -45,17 +133,18 @@ async function fetchAdsFromLink(page, adLink) {
         isFirstRun = true;
     }
 
-    const newAds = ads.filter(ad => !previousAds.includes(ad));
+    let adsToSend = [];
+
+    for (const ad of ads) {
+        if (!previousAds.includes(ad) && await isNewAdvertisement(page, ad)) {
+            adsToSend.push(ad);
+        }
+    }
 
     fs.writeFileSync(fileName, ads.join('\n'), 'utf-8');
 
-    if (newAds.length && !isFirstRun) {
-        console.log(`New advertisements found for ${adLink}:`, newAds);
-        for (const ad of newAds) {
-            await axios.post(DISCORD_WEBHOOK_URL, {
-                content: `New listing found: ${ad}`
-            });
-        }
+    if (adsToSend.length && !isFirstRun) {
+        await sendToDiscord(adsToSend);
     } else {
         console.log(`No new advertisements found for ${adLink}.`);
     }
@@ -65,13 +154,17 @@ async function fetchAdsFromLink(page, adLink) {
     const browser = await puppeteer.launch({ headless: false });
     const page = await browser.newPage();
     
-    // Log into Facebook once
     await loginToFacebook(page);
+    console.log('Logged in!');
 
-    // Loop through each link and fetch the ads
-    for (const adLink of AD_LINKS) {
-        await fetchAdsFromLink(page, adLink);
+    while (true) {
+        await pauseIfMidnightTo7am();
+
+        for (const adLink of AD_LINKS) {
+            await fetchAdsFromLink(page, adLink);
+        }
+
+        console.log("Waiting for 4 minutes before checking the links again.");
+        await page.waitForTimeout(240000);
     }
-
-    await browser.close();
 })();
